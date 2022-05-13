bce92159ab572f7eff5a3ad4b16ab092b3ec16d1
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-05-13 16:41:29 -0400

    updated duration

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -54,7 +54,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
               <CountUp
                 start={0}
                 end={recovered.value}
-                duration={2.5}
+                duration={3.5}
                 separator=","
               />
             </Typography>
