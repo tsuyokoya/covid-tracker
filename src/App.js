d88ceb6103f59c2588da181ad3c69549731f46ec
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-05-13 16:39:39 -0400

    Add handleCountryChange and updated render method

--- src/App.js
+++ src/App.js
@@ -2,22 +2,32 @@ import React from "react";
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
 import { fetchData } from "./api";
+import covidImage from "./images/covid-image.jpeg";
 
 class App extends React.Component {
   state = {
     data: {},
+    country: "",
   };
   async componentDidMount() {
     const fetchedData = await fetchData();
     this.setState({ data: fetchedData });
   }
+
+  handleCountryChange = async (country) => {
+    //fetch the data
+    const fetchedData = await fetchData(country);
+    //set the state
+    this.setState({ data: fetchedData, country: country });
+  };
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
     return (
       <div className={styles.container}>
+        <img className={styles.image} src={covidImage} alt="COVID-19" />
         <Cards data={data} />
-        <CountryPicker />
-        <Chart />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
       </div>
     );
   }
