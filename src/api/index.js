dae7654b2706e350a078909fdafa6dfd29fcc2ae
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-05-13 14:32:53 -0400

    destructure for simpler syntax

--- src/api/index.js
+++ src/api/index.js
@@ -4,13 +4,15 @@ const url = "https://covid19.mathdro.id/api";
 
 export const fetchData = async () => {
   try {
-    const { data } = await axios.get(url);
-    const modifiedData = {
-      confirmed: data.confirmed,
-      recovered: data.recovered,
-      deaths: data.deaths,
-      lastUpdate: data.lastUpdate,
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
+
+    return {
+      confirmed,
+      recovered,
+      deaths,
+      lastUpdate,
     };
-    return modifiedData;
   } catch (err) {}
 };
