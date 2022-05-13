5d9dffbe56e396b2f27c69dc6e13d89746d76579
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-05-13 14:16:39 -0400

    Created necessary components

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+// This file takes all components and exports it from the components folder
+export { default as Cards } from "./Cards/Cards";
+export { default as Chart } from "./Chart/Chart";
+export { default as CountryPicker } from "./CountryPicker/CountryPicker";
