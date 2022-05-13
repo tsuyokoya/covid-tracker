e85d2d826e85d61391a797eb14bfa27c41716e40
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-05-13 16:42:57 -0400

    import chart from chart.js

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -5,13 +5,12 @@ import { Chart as ChartJS } from "chart.js/auto";
 
 import styles from "./Chart.module.css";
 
-const Chart = () => {
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
   const [dailyData, setDailyData] = useState([]);
   useEffect(() => {
     const fetchAPI = async () => {
       setDailyData(await fetchDailyData());
     };
-    console.log(dailyData);
     fetchAPI();
   }, []);
 
@@ -37,7 +36,33 @@ const Chart = () => {
       }}
     />
   ) : null;
-  return <div className={styles.container}>{lineChart}</div>;
+
+  const barChart = confirmed ? (
+    <Bar
+      data={{
+        labels: ["Infected", "Recovered", "Deaths"],
+        datasets: [
+          {
+            label: "People",
+            backgroundColor: [
+              "rgba(0,0,255,0.5)",
+              "rgba(0,255,0,0.5)",
+              "rgba(255,0,0,0.5)",
+            ],
+            data: [confirmed.value, recovered.value, deaths.value],
+          },
+        ],
+      }}
+      options={{
+        legend: { display: false },
+        title: { display: true, text: `Current state in ${country}` },
+      }}
+    />
+  ) : null;
+
+  return (
+    <div className={styles.container}>{country ? barChart : lineChart}</div>
+  );
 };
 
 export default Chart;
